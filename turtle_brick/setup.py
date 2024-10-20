a3d3727cef1db861174667bb19f83740adf7c2d1
sairam-umakanth <sairamumakanth2028@u.northwestern.edu>
2024-10-20 17:28:58 -0500

    Functioning URDF file

---
+++ turtle_brick/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtle_brick'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml', 
+                                   'launch/show_turtle.launch.xml',
+                                   'urdf/turtle.urdf.xacro']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sairamu02',
+    maintainer_email='sairamumakanth2028@u.northwestern.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
